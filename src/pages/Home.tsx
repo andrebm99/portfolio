9f32538480d9fe187c8391bef49e23c74aa1d10a
andrebm99 <ronaldobayona65@gmail.com>
2025-12-18 12:31:59 -0500

    feat: sección principal terminada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,29 +2,36 @@ import "../css/home.css";
 
 export default function Home() {
   return (
-    <section className="caja">
-      <div className="contenido">
-        <p className="texto-ini">Hola, mi nombre es</p>
-        <h2>André Bayona</h2>
-        <div className="caja-presentacion">
-          <p>
-          <span>
-            Soy estudiante de Ingeniería de Software con enfoque en desarrollo <br />
-            Backend, con interés en buenas prácticas, arquitectura y rendimiento.
-          </span>
-        </p>
-        </div>
-        <div className="caja-redes">
-          <div className="centrando-redes">
-            <a href="https://github.com/andrebm99" target="_blank">
-              <i className="fa-brands fa-github"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/andrebayona/" target="_blank">
-              <i className="fa-brands fa-linkedin-in"></i>
-            </a>
+    <main>
+      <section className="caja">
+        <div className="contenido">
+          <p className="texto-ini">Hola, mi nombre es</p>
+          <h2>André Bayona</h2>
+          <div className="caja-presentacion">
+            <p>
+              <span>
+                Soy estudiante de Ingeniería de Software con enfoque en
+                desarrollo <br />
+                Backend, con interés en buenas prácticas, arquitectura y
+                rendimiento.
+              </span>
+            </p>
+          </div>
+          <div className="caja-redes">
+            <div className="centrando-redes">
+              <a href="https://github.com/andrebm99" target="_blank">
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/andrebayona/"
+                target="_blank"
+              >
+                <i className="fa-brands fa-linkedin-in"></i>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </main>
   );
 }
