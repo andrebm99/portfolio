b390e8be0f14122f5d600e8c43839d2295f5563b
andrebm99 <ronaldobayona65@gmail.com>
2025-12-19 13:22:38 -0500

    feat: pagina principal terminada

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,15 @@
 export default function Footer() {
   return (
-    <footer className="bg-neutral-primary-soft rounded-base shadow-xs border border-white rounded-2xl m-4">
-      <div className="w-full mx-auto max-w-7xl p-4 md:flex md:items-center md:justify-between">
+    <footer className="bg-neutral-primary-soft rounded-base shadow-xs border-t shadow-lg">
+      <div className="w-full mx-auto max-w-7xl p-10 md:flex md:items-center md:justify-between">
         <span className="text-sm text-white text-body sm:text-center">
-          © 2023{" "}
+          © 2025{" "}
           <a href="https://flowbite.com/" className="hover:underline">
             André Bayona™
           </a>
           . Todos los derechos reservados.
         </span>
-        <ul className="flex flex-wrap text-white items-center mt-3 text-sm font-medium text-body sm:mt-0">
+        <ul className="flex flex-wrap text-white items-center mt-3 text-lg font-medium text-body sm:mt-0">
           <li>
             <a href="https://www.linkedin.com/in/andrebayona/" className="hover:underline me-4 md:me-6" target="_blank">
               <i className="fa-brands fa-linkedin-in"></i>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import "../css/home.css";
-import imgPerfil from "../assets/perfil.jpg"
-
+import imgPerfil from "../assets/perfil.jpg";
 
 export default function Home() {
   return (
     <main>
-      
       <section className="caja">
         <div className="contenido">
           <p className="texto-ini">Hola, mi nombre es</p>
@@ -36,7 +34,10 @@ export default function Home() {
         </div>
       </section>
       <section className="caja flex flex-col md:flex-row">
-        <div className="w-full md:w-1/2 p-4 items-center justify-center flex" id="aboutme">
+        <div
+          className="w-full md:w-1/2 p-4 items-center justify-center flex"
+          id="aboutme"
+        >
           <div className="about">
             <h3 className="">Sobre mi</h3>
             <p>
@@ -55,9 +56,83 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div className="w-full md:w-1/2 p-4 items-center justify-center flex" id="aboutme">
+        <div
+          className="w-full md:w-1/2 p-4 items-center justify-center flex"
+          id="aboutme"
+        >
           <div className="about">
-            <img src={imgPerfil} alt="André Bayona"/>
+            <img src={imgPerfil} alt="André Bayona" />
+          </div>
+        </div>
+      </section>
+      <section className="caja-skills">
+        <div className="titulo-skills">
+          <h3>Skills</h3>
+        </div>
+        <div className="seccion-skills">
+          <div className="shortcut">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuHnJDLOcdm_0b6N6kNj-1OvO9KhKYgqIy0w&s"
+              alt="JavaScript"
+            />
+            JavaScript
+          </div>
+          <div className="shortcut">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png"
+              alt="TypeScript"
+            />
+            TypeScript
+          </div>
+          <div className="shortcut">
+            <img
+              src="https://docs.nestjs.com/assets/logo-small-gradient.svg"
+              alt="NestJS"
+            />
+            NestJS
+          </div>
+          <div className="shortcut">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Angular_gradient_logo.png/1200px-Angular_gradient_logo.png"
+              alt="Angular"
+            />
+            Angular
+          </div>
+          <div className="shortcut">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png"
+              alt="React"
+            />
+            React
+          </div>
+
+          <div className="shortcut">
+            <img
+              src="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo.png"
+              alt="Java"
+            />
+            Java
+          </div>
+          <div className="shortcut">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Spring_Boot.svg/1200px-Spring_Boot.svg.png"
+              alt="Spring Boot"
+            />
+            Spring Boot
+          </div>
+          <div className="shortcut">
+            <img
+              src="https://images.icon-icons.com/2415/PNG/512/mysql_original_wordmark_logo_icon_146417.png"
+              alt="JavaScript"
+            />
+            MySQL
+          </div>
+          <div className="shortcut">
+            <img
+              src="https://images.icon-icons.com/2415/PNG/512/postgresql_plain_wordmark_logo_icon_146390.png"
+              alt="PostgreSQL"
+            />
+            PostgreSQL
           </div>
         </div>
       </section>
